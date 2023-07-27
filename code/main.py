23f2d78bad54069322160bd54468cdf614617708
kazonhavel1 <joaopedroescola63@gmail.com>
2023-07-26 22:36:27 -0300

    Versão 0.2 Funções Ola e Dado de 20 números

--- code/main.py
+++ code/main.py
@@ -1,15 +1,25 @@
 import discord
+import random
 
 # Defina as intenções que o bot irá utilizar
 intents = discord.Intents.default()
 intents.message_content = True
+intents.typing = False
+intents.presences = False
+intents.members = True
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logado como {0}!'.format(self.user))
-
-    async def on_message(self, message):
-        print('Mensagem do autor {0.author}: {0.content}'.format(message))
-
+        
+    async def on_message(self,message):
+        if message.content  == '!ola':
+            await message.channel.send (f'Olá, meu nome é {self.user}, é um prazer {message.author}' )
+        elif message.content == '!r 1d20':
+            numero = random.randint (1,20)
+            await message.channel.send (f'Saiu o número {numero}!')      
+        else:{
+         print('Mensagem do autor {0.author}: {0.content}'.format(message))}
+            
 client = MyClient(intents=intents)  # Passa o objeto de intenções ao criar a instância do cliente
-client.run('') # TOKEN de acesso do BOT aqui
+client.run('MTEzMzkxMzc5NzIxMjUyMDQ3OA.GUtZeI.G1wA2TJ-FXxwXAPa6MXjTR-1VzjqUiO3WSsKTE') # TOKEN de acesso do BOT aqui
