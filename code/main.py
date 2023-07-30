b6db50c1d943aeef9323ad2188916e8bfa4eb571
kazonhavel1 <joaopedroescola63@gmail.com>
2023-07-30 13:49:23 -0300

    API de Cotaçao da Moeda USD/EUR

--- code/main.py
+++ code/main.py
@@ -1,6 +1,7 @@
 import discord
 import random
 from decouple import config
+import requests
 
 # Defina as intenções que o bot irá utilizar
 intents = discord.Intents.default()
@@ -12,9 +13,17 @@ intents.members = True
 SECRET_KEY = config("SECRET_KEY")
 
 class MyClient(discord.Client):    
+    
+    def CotacaoMoeda(moeda):
+        #moeda = 'USD'
+        cotacoes = requests.get(f"https://economia.awesomeapi.com.br/json/last/{moeda}-BRL")
+        cotacoes = cotacoes.json()
+        cotacaorecebida = float(cotacoes[f'{moeda}BRL']['bid'])
+        return (f'A cotação da moeda {moeda} é R$ {round(cotacaorecebida,2)}.')
+         
+        
     async def on_ready(self):
         print('Logado como {0}!'.format(self.user))
-        print(f'Variavel com o valor: {SECRET_KEY}')
         
     async def on_message(self,message):
         if message.content  == '!ola':
@@ -25,10 +34,17 @@ class MyClient(discord.Client):
                 await message.channel.send (f'Critou! O número é {numero}')
                 return
             else:
-                await message.channel.send (f'O número é {numero}')
-        
+                await message.channel.send (f'O número é {numero}')      
+        elif message.content == '!USD':
+               cotacao = MyClient.CotacaoMoeda('USD')
+               await message.channel.send (f'{cotacao}')
+        elif message.content == '!EUR':
+               cotacao = MyClient.CotacaoMoeda('EUR')
+               await message.channel.send (f'{cotacao}')
         else:{
          print('Mensagem do autor {0.author}: {0.content}'.format(message))}
+        
+        
             
 client = MyClient(intents=intents)  # Passa o objeto de intenções ao criar a instância do cliente
 client.run(f'{SECRET_KEY}')  # TOKEN de acesso do BOT aqui
